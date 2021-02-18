1205adc6ba2850fcd8f206126dd49944bea43799
Irineo Ramos <irineo.ramos@codeimmersives.com>
2021-02-18 14:56:54 -0600

    all functions pass.

---
+++ main.js
@@ -0,0 +1,26 @@
+function question3(str){
+    return str + "???";
+}
+
+function spc( str1,str2){
+    return str1 + " " + str2;
+}
+
+function sub(num1,num2,num3){
+    return num1 - num2 -num3 ;
+}
+
+function subRevOrd(num1,num2,num3){
+    return num3 - num2- num1;
+}
+
+function triple(str){
+    return str + str + str;
+}
+
+function findSqrt(num){
+    return Math.sqrt(num) 
+}
+
+const result = findSqrt(30);
+result; 
